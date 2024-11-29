e00a3baf9606b3be2bc7ab99e910482068973669
ifzat <nifzat@gmail.com>
2024-11-29 14:19:21 +1300

    update

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ class AssistantUI:
         # Display assistant description
         assistant_descriptions = {
             "BARONIA_B": "https://f001.backblazeb2.com/file/KioskOrtanaProxy/CACA2CC9-E851-469B-851B-0FFE78D90A45.MP4",
-            "FACE_OF_MALAYSIA": "https://f001.backblazeb2.com/file/KioskOrtanaProxy/CACA2CC9-E851-469B-851B-0FFE78D90A45.MP4",
+            "FACE_OF_MALAYSIA": "https://f001.backblazeb2.com/file/KioskOrtanaProxy/6C985F9C-8729-4A5F-A661-5F16E32F7EB0.MP4",
             "MENJUNJUNG_KASIH": "https://f001.backblazeb2.com/file/KioskOrtanaProxy/A5118C34-0CC0-46FD-901D-FD3D2587BFC9.MP4"
         }
         st.markdown(f"<div class='assistant-info'>{assistant_descriptions.get(selected_assistant_name, '')}</div>", 
